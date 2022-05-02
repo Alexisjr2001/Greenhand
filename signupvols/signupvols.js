b9d9c7a4e6d3963605ea69653088a134e9eb64b5
safentisAuth <safentis@csd.auth.gr>
2022-05-02 22:54:23 +0300

    Rename files

--- signupvols/signupvols.js
+++ signupvols/signupvols.js
@@ -1,6 +1,6 @@
 function redirect() {
     document.querySelector('body').style.opacity = 0
     setTimeout(function() { 
-        window.location.href = "../signuporgs/index.html";
+        window.location.href = "../signuporgs/signuporgs.html";
     }, 500)
 }

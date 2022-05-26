48f59d4edc1dfa89ddabef8dfcf8df5ec37fa325
GeorgePapoulidis <gpapoulid@csd.auth.gr>
2022-05-26 23:13:53 +0300

    added an image for users that don't select any image for their profile data is now stored in database after all connections and input is checked created a folder for all the php files that are supposed to be included in other php files

--- SignUp/SignUp.php
+++ SignUp/SignUp.php
@@ -1,4 +1,9 @@
 <?php
+function exitPHP($GETParams){
+    $urlQuery=http_build_query($GETParams);
+    header("Location: ..\SignUp\SignUp.html?$urlQuery");
+    exit();
+}
 if (isset($_POST["submit"])){
     $urlQuery=array();
 
@@ -77,42 +82,89 @@ if (isset($_POST["submit"])){
         $urlQuery["phone"]="NaN";
     }
 
-    $file = $_FILES["photo"];
-    $fileName = $_FILES["photo"]["name"];
-    $fileTmpName = $_FILES["photo"]["tmp_name"];
-    $fileSize = $_FILES["photo"]["size"];
-    $fileError = $_FILES["photo"]["error"];
-    $fileType = $_FILES["photo"]["type"];
+    if ((isset($_FILES["photo"]) and empty($_FILES['photo']['name'])) or !(isset($_FILES["photo"]))){
+        $fileDestination="../Assets/empty-user.png";
+    }
+    else{
+        $file = $_FILES["photo"];
+        $fileName = $_FILES["photo"]["name"];
+        $fileTmpName = $_FILES["photo"]["tmp_name"];
+        $fileSize = $_FILES["photo"]["size"];
+        $fileError = $_FILES["photo"]["error"];
+        $fileType = $_FILES["photo"]["type"];
 
 
-    $fileExt = explode(".",$fileName);
-    $fileActualExt = strtolower(end($fileExt));
+        $fileExt = explode(".",$fileName);
+        $fileActualExt = strtolower(end($fileExt));
 
-    $allowed=array("jpg","jpeg","png");
+        $allowed=array("jpg","jpeg","png");
 
-    if (in_array($fileActualExt,$allowed)){
-        if($fileError===0){
-            if($fileSize<25000000){//25MB
-                $fileNameNew = uniqid('',true).".".$fileActualExt;
-                $fileDestination = '../Assets/'.$fileNameNew;
-                move_uploaded_file($fileTmpName,$fileDestination);
+        if (in_array($fileActualExt,$allowed)){
+            if($fileError===0){
+                if($fileSize<25000000){//25MB
+                    $fileNameNew = uniqid('',true).".".$fileActualExt;
+                    $fileDestination = '../Assets/'.$fileNameNew;
+                    move_uploaded_file($fileTmpName,$fileDestination);
+                }
+                else{
+                    $urlQuery["photo"]="too-big";
+                }
             }
             else{
-                $urlQuery["photo"]="too-big";
+                $urlQuery["photo"]="err";
             }
         }
         else{
-            $urlQuery["photo"]="err";
+            $urlQuery["photo"]="inv-ext";
         }
-    }
-    else{
-        $urlQuery["photo"]="inv-ext";
     }
 
     if (count($urlQuery)>0){
-        $urlQuery=http_build_query($urlQuery);
-        header("Location: ..\SignUp\SignUp.html?$urlQuery");
-        exit();
+        exitPHP($urlQuery);
     }
+
+    include_once "../includes/DBConn.inc.php";
+    if(mysqli_connect_errno()){
+        $urlQuery["server"]="conn-error";
+        exitPHP($urlQuery);
+    }
+    if(!mysqli_ping($conn)){
+        $urlQuery["server"]="conn-dead";
+        exitPHP($urlQuery);
+    }
+
+    $stmt=mysqli_stmt_init($conn);
+
+    $checkUniqueness='SELECT * FROM users WHERE users.usersEmail=?;';
+    if(!mysqli_stmt_prepare($stmt,$checkUniqueness)){
+        $urlQuery["server"]="error";
+        exitPHP($urlQuery);
+    }
+    mysqli_stmt_bind_param($stmt,"s",$email);
+    mysqli_stmt_execute($stmt);
+    $results=mysqli_stmt_get_result($stmt);
+
+    if(mysqli_num_rows($results)>0){
+        $urlQuery["email"]="exists";
+        exitPHP($urlQuery);
+    }
+    if(isset($age) and isset($gender)){
+        $insertAccount='INSERT INTO users (usersEmail,usersPwd,usersName,usersPhone,usersCountry,usersCity,usersImage,usersAge,usersGender) values(?,?,?,?,?,?,?,?,?);';
+    }
+    else{
+        $insertAccount='INSERT INTO users (usersEmail,usersPwd,usersName,usersPhone,usersCountry,usersCity,usersImage) values(?,?,?,?,?,?,?);';
+    }
+    if(!mysqli_stmt_prepare($stmt,$insertAccount)){
+        $urlQuery["server"]="insert-error";
+        exitPHP($urlQuery);
+    }
+    if(isset($age) and isset($gender)){
+        mysqli_stmt_bind_param($stmt,"sssisssis",$email,$pwd,$name,$phone,$country,$city,$fileDestination,$age,$gender);
+    }
+    else{
+        mysqli_stmt_bind_param($stmt,"sssisss",$email,$pwd,$name,$phone,$country,$city,$fileDestination);
+    }
+    mysqli_stmt_execute($stmt);
     header("Location: SignUp.html?acc=success");
+
 }

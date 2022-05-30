d9e1bcb89775c11d466b0955a882532cfc5ba329
Alexisjr2001 <atsingos@csd.auth.gr>
2022-05-30 17:39:31 +0300

    Added: Footer is now included, where it is used. Modified: Article.php Fixes: Minor visual fixes

--- About_Us/AboutUs.php
+++ About_Us/AboutUs.php
@@ -54,31 +54,14 @@ include_once '../includes/Header.inc.php';
                                                 src="https://images.unsplash.com/photo-1559027615-cd4628902d4a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dm9sdW50ZWVyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
                                                 alt="image of a volunteer">
                 <a href="../Index/index.php">
-                    <button>Join Us!</button>
+                    <button id="join-us">Join Us!</button>
                 </a>
             </div>
         </div>
     </section>
-    <footer id="contact-us">
-        <div id="footer-title-container">
-            <h1><b>Contact Us!</b></h1>
-        </div>
-        <div id="footer-details-container">
-            <div id="contact-info">
-                <p>Telephone: +30 123123123</p>
-                <p>Email: contact-us@greenhand.com</p>
-                <p>Street Address: Fictional Street 15</p>
-            </div>
-            <div id="follow-us">
-                <div id="follow-us-title"><b>Follow Us!</b></div>
-                <div id="follow-us-links">
-                    <a href=""><img src="../Assets/facebook-logo.png" alt=""></a>
-                    <a href=""><img src="../Assets/twitter-logo.png" alt=""></a>
-                    <a href=""><img src="../Assets/youtube-logo.png" alt=""></a>
-                </div>
-            </div>
-        </div>
-    </footer>
 </main>
+<?php
+include_once '../includes/Footer.inc.php';
+?>
 </body>
 </html>
---
+++ includes/Footer.inc.php
@@ -0,0 +1,20 @@
+<footer id="contact-us">
+    <div id="footer-title-container">
+        <h1><b>Contact Us!</b></h1>
+    </div>
+    <div id="footer-details-container">
+        <div id="contact-info">
+            <p>Telephone: (+30) 123123123</p>
+            <p>Email: contact-us@greenhand.com</p>
+            <p>Street Address: Fictional Street 15</p>
+        </div>
+        <div id="follow-us">
+            <div id="follow-us-title"><b>Follow Us!</b></div>
+            <div id="follow-us-links">
+                <a href="https://www.facebook.com/"><img src="../Assets/facebook-logo.png" alt="Facebook logo"></a>
+                <a href="https://twitter.com/"><img src="../Assets/twitter-logo.png" alt="Twitter Logo"></a>
+                <a href="https://www.youtube.com/"><img src="../Assets/youtube-logo.png" alt="Youtube Logo"></a>
+            </div>
+        </div>
+    </div>
+</footer>
